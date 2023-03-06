13d8f27a3fa10aeaeb85b13f0e7520c6dee643d4
Yanggum <bak405@naver.com>
2023-03-07 05:15:53 +0900

    connector 수정

--- api/connector.js
+++ api/connector.js
@@ -1,9 +1,10 @@
 const mariadb = require('mariadb');
+const env = require('../env');
 // read info form env
-const dbhost = process.env.DBHOST;
-const dbuser = process.env.DBUSER;
-const dbpassword = process.env.DBPASSWORD;
-const db = process.env.DB;
+const dbhost = env.mariaDB.DBHOST;
+const dbuser = env.mariaDB.DBUSER;
+const dbpassword = env.mariaDB.DBPASSWORD;
+const db = env.mariaDB.DB;
 const connector = mariadb.createPool({
     host: dbhost,
     user: dbuser,
